c40d23ac52506148d0691e8b9cc34aebc4083926
Codeur-Omniscient <anthonymedin258@outlook.fr>
2026-01-06 16:56:21 +0100

    Refactor UserService to implement user creation, retrieval by ID, and add methods for finding users by email and phone number

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(user);
   }
 
   findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findById(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return `This action updates a #${id} user with ${JSON.stringify(updateUserDto)}`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async findOneByEmail(email: string) {
+    const existingUser = await this.userRepository.findOneBy({ email });
+    return existingUser;
+  }
+
+  async findByPhoneNumber(phoneNumber: string) {
+    const existingUser = await this.userRepository.findOneBy({ phoneNumber });
+    return existingUser;
   }
 }
